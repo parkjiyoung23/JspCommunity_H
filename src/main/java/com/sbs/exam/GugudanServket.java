1b377d7d7f332414db187f49108d2cc78b46f918
parkjiyoung23 <dnixpobe2@naver.com>
2022-09-15 21:08:09 +0900

    구구단에 model2 방식 적용

--- src/main/java/com/sbs/exam/GugudanServket.java
+++ src/main/java/com/sbs/exam/GugudanServket.java
@@ -1,25 +1,30 @@
 package com.sbs.exam;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+
 @WebServlet("/gugudan")
 public class GugudanServket extends HttpServlet {
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)  {
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
     Rq rq = new Rq(req,resp);
 
     int dan = rq.getIntParam("dan",9);
     int limit = rq.getIntParam("limit",9);
 
-    rq.appendBody("<h1>%d단<h1>\n".formatted(dan));
-    for(int i=1; i<=limit; i++){
+    req.setAttribute("dan",dan);
+    req.setAttribute("limit",limit);
 
-      rq.appendBody("<div>%d * %d = %d</div>".formatted(dan, i, dan * i));
-    }
+    RequestDispatcher requestDispatcher = req.getRequestDispatcher("/gugudan2.jsp");
+    requestDispatcher.forward(req,resp);
   }
 }
 
